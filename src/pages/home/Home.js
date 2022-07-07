518f0eb67b9389b264074f6566b8c6c2df7d5094
shourovahmd <moh.abuhanifa@gmail.com>
2022-07-08 01:44:25 +0600

    footer added

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -1,6 +1,7 @@
 import Featured from "../../components/featured/Featured";
 import FeaturedProperties from "../../components/featuredProperties/FeaturedProperties";
 import Header from "../../components/header/Header";
+import MailList from "../../components/mailList/MailList";
 import Navbar from "../../components/navbar/Navbar";
 import PropertyList from "../../components/propertyList/PropertyList";
 import "./home.css";
@@ -15,6 +16,7 @@ const Home = () => {
         <PropertyList/>
         <h1 className="homeTitle">Homes guests love</h1>
         <FeaturedProperties/>
+        <MailList/>
       </div>
     </div>
   );
