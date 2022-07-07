21ca265215bc6ee2efbebca86602a4fb732c9292
shourovahmd <moh.abuhanifa@gmail.com>
2022-07-08 02:38:26 +0600

    updating list

--- src/pages/list/List.js
+++ src/pages/list/List.js
@@ -1,11 +1,18 @@
 import Header from "../../components/header/Header";
 import Navbar from "../../components/navbar/Navbar";
-
+import "./list.css";
 const List = () => {
   return (
     <div>
       <Navbar />
       <Header type="list" />
+      <div className="listContainer">
+        <div className="listWrapper">
+          <div className="listSearch">
+            <h1 className="lsTitle">Search</h1>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
