b0b006e4a4edac4399b6345367145d2b07e26886
shourovahmd <moh.abuhanifa@gmail.com>
2022-07-08 01:08:03 +0600

    featured done

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -1,3 +1,4 @@
+import Featured from "../../components/featured/Featured";
 import Header from "../../components/header/Header";
 import Navbar from "../../components/navbar/Navbar";
 import "./home.css";
@@ -5,7 +6,11 @@ const Home = () => {
   return (
     <div>
       <Navbar />
-      <Header/>
+      <Header />
+      <div className="homeContainer">
+        <Featured />
+        <h1 className="homeTitle">Browse by property type</h1>
+      </div>
     </div>
   );
 };
