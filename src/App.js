9a927cca38429056302f3db097f06522ec1f9601
shourovahmd <moh.abuhanifa@gmail.com>
2022-07-07 19:52:49 +0600

    updadted

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "./App.css";
-import Hotel from "./hotel/Hotel";
-import List from "./list/List";
-import Home from "./pages/Home";
+import Home from "./pages/home/Home";
+import Hotel from "./pages/hotel/Hotel";
+import List from "./pages/list/List";
 
 function App() {
   return (
