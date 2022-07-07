51adf478303647a4f7894abc777e58b4e8365ac2
shourovahmd <moh.abuhanifa@gmail.com>
2022-07-08 01:32:13 +0600

    mailList added

---
+++ src/components/mailList/MailList.js
@@ -0,0 +1,11 @@
+import "./mailList.css";
+
+const MailList = () => {
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default MailList;
--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -1,4 +1,5 @@
 import Featured from "../../components/featured/Featured";
+import FeaturedProperties from "../../components/featuredProperties/FeaturedProperties";
 import Header from "../../components/header/Header";
 import Navbar from "../../components/navbar/Navbar";
 import PropertyList from "../../components/propertyList/PropertyList";
@@ -13,7 +14,7 @@ const Home = () => {
         <h1 className="homeTitle">Browse by property type</h1>
         <PropertyList/>
         <h1 className="homeTitle">Homes guests love</h1>
-        
+        <FeaturedProperties/>
       </div>
     </div>
   );
