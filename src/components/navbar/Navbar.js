25ac5e55cc26f4d56f890291fdf64a3303224b7f
shourovahmd <moh.abuhanifa@gmail.com>
2022-07-08 21:17:32 +0600

    navbar updated

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -1,9 +1,12 @@
+import { Link } from "react-router-dom";
 import "./navbar.css";
 const Navbar = () => {
   return (
     <div className="navbar">
       <div className="navContainer">
-        <span className="logo">BookingMaster</span>
+        <Link to="/" style={{ color: "inherit", textDecoration: "none" }}>
+          <span className="logo">BookingMaster</span>
+        </Link>
         <div className="navItems">
           <button className="navButton">Register</button>
           <button className="navButton">Login</button>
