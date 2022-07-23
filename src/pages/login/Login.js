21c9fcf4812c360cdb43e55f0806a08c4388e200
shourovahmd <moh.abuhanifa@gmail.com>
2022-07-24 02:06:59 +0600

    users fetched successfully

--- src/pages/login/Login.js
+++ src/pages/login/Login.js
@@ -21,8 +21,11 @@ const Login = () => {
     e.preventDefault();
     dispatch({ type: "LOGIN_START" });
     try {
-      const res = await axios.post("http://localhost:5000/api/auth/login", credentials)
-      dispatch({ type: "LOGIN_SUCCESS", payload: res.data });
+      const res = await axios.post(
+        "http://localhost:5000/api/auth/login",
+        credentials
+      );
+      dispatch({ type: "LOGIN_SUCCESS", payload: res.data.details });
       navigate("/");
     } catch (err) {
       dispatch({ type: "LOGIN_FAILURE", payload: err.response });
